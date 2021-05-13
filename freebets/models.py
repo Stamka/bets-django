041920a425fa9cfd4aad0f1d41b421b1cb477173
Stamka <zik380@mail.ru>
2021-05-13 21:40:11 +0300

    fix default and min values for sql
    Добавил ограничение и дефолтные параметры для создания евентов в БД

--- freebets/models.py
+++ freebets/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.core.validators import MaxValueValidator, MinValueValidator
 import datetime
 
 
@@ -7,10 +8,10 @@ import datetime
 class Event(models.Model):
     event_name = models.CharField('Game name', max_length=200) # название матча например зенит-спартак
     event_date = models.DateTimeField('Game date') # дата события
-    event_result = models.IntegerField() # 1 first team win - 2 second team win 0 - pari isnt finish yet
+    event_result = models.IntegerField('event result', default=0,validators=[MaxValueValidator(2), MinValueValidator(0)]) # 1 first team win - 2 second team win 0 - pari isnt finish yet
     # эти две переменные для расчёта коэфициента
-    cash_to_first_team = models.IntegerField() # сколько денег поставили на первую команду
-    cash_to_second_team = models.IntegerField() # на вторую команду
+    cash_to_first_team = models.IntegerField(validators=[MinValueValidator(0)], default=0) # сколько денег поставили на первую команду
+    cash_to_second_team = models.IntegerField(validators=[MinValueValidator(0)], default=0) # на вторую команду
 
     def __str__(self):
         return self.event_name
@@ -22,7 +23,7 @@ class Event(models.Model):
 class Bet(models.Model):
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
     eventBet = models.CharField('pari', max_length=200)
-    dollars = models.IntegerField('bet amount')
+    dollars = models.IntegerField('bet amount',validators=[MinValueValidator(0)])
     date_added = models.DateTimeField('bet date')
 
     def __str__(self):
@@ -30,7 +31,7 @@ class Bet(models.Model):
 
 class User(models.Model):
     login = models.CharField('login',max_length=200)
-    cash = models.IntegerField()
+    cash = models.IntegerField(validators=[MinValueValidator(0)])
     registration = models.DateTimeField('Registration date')
 
     def __str__(self):
