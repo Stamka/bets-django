28a6c33a42bf0f88407da967f33ae1bd1048cc2b
rozdestvenskiy <dimar08092@gmail.com>
2021-05-14 02:38:24 +0300

    pre_release

--- freebets/views.py
+++ freebets/views.py
@@ -3,7 +3,63 @@ from .models import Event,Bet, User
 from django.utils import timezone
 from django.http import Http404, HttpResponseRedirect
 from django.urls import reverse
+
+def check_events():
+    for a in Event.objects.all():
+        #print(a.event_name)
+        #print("status = ", a.event_result)
+        cash1 = a.cash_to_first_team
+        cash2 = a.cash_to_second_team
+        user =User.objects.get(id= 1)
+        if cash1 == cash2:
+            kef1=2
+            kef2=2
+        else:
+            s = cash1+cash2
+            print(cash1, cash2, s)
+            try:
+                kef1 = 1 /(cash1/s)
+            except:
+                kef1 = 1 / (0.001 /s)
+            try:
+                kef2 = 1 / (cash2/s)
+            except:
+                kef2 = 1 / (0.001 /s)
+            if (kef2>10):
+                kef2=10
+            kef1 = float('{:.2f}'.format(kef1))
+            kef2 = float('{:.2f}'.format(kef2))
+        if a.event_result != 0:
+            if a.event_result == 1:
+                #print("xxx")
+                a.cash_to_first_team += a.cash_to_second_team
+                a.cash_to_second_team = 0
+                a.save()
+                for i in a.bet_set.all():
+                    #print(i.eventBet)
+                    if i.eventBet == "first":
+                        #print("user cash now = ", user.cash)
+                        if a.cash_to_first_team >= int(i.dollars * kef1):
+                            user.cash += int(i.dollars * kef1)
+                            #print("user cash now = ", user.cash)
+                            user.save()
+                            a.cash_to_first_team -= int(i.dollars * kef1)
+                            a.save()
+            if a.event_result == 2:
+                a.cash_to_second_team += a.cash_to_first_team
+                a.cash_to_first_team = 0
+                a.save()
+                for i in a.bet_set.all():
+                    if i.eventBet == "second":
+                        if a.cash_to_second_team >= int(i.dollars * kef2):
+                            user.cash += int(i.dollars * kef2)
+                            user.save()
+                            a.cash_to_second_team -= int(i.dollars * kef2)
+                            a.save()
+
+
 def index(request):
+    check_events()
     new_bets_list = Event.objects.order_by('-event_date')
     user =User.objects.get(id= 1)
     return render(request, 'bets.html',{'new_bets':new_bets_list, 'user':user})
@@ -40,7 +96,8 @@ def event(request, event_id, alert=0):
         kef1 = float('{:.2f}'.format(kef1))
         kef2 = float('{:.2f}'.format(kef2))
     latest_bets = a.bet_set.order_by('-id')[:10]
-    
+    check_events()
+    '''''
     if a.event_result != 0:
         if a.event_result == 1:
             for i in a.bet_set.all():
@@ -56,14 +113,15 @@ def event(request, event_id, alert=0):
                     if cash1 >= int(i.dollars * kef2):
                         user.cash += int(i.dollars * kef2)
                         user.save()
-                        a.cash_to_second_team -= int(i.dollars * kef1)
+                        a.cash_to_second_team -= int(i.dollars * kef2)
                         a.save()
                 
-
+'''
     return render(request, 'event.html', {'event': a, 'latest_bets' : latest_bets, 'user': user,
                                           'k1': kef1,'k2': kef2,'cash1':cash1,'cash2': cash2 })
 
 def feel_cash(request):
+    check_events()
     b = User.objects.get(id=1)
     dollars = request.POST['feel_money']
     if int(dollars) >= 0:
@@ -74,6 +132,7 @@ def feel_cash(request):
         raise Http404(("BAN!!!" * 100 + "\n") * 100)
 
 def make_bet(request, event_id):
+    check_events()
     try:
         a=Event.objects.get( id = event_id)
 
